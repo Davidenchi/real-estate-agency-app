efa4b4bde7f1a2de79d7777961b80fdf37adbc8d
Davidenchi <david_petrov@abv.bg>
2025-02-05 19:34:29 +0200

    test

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -14,20 +14,6 @@ function HomePage() {
             repellat provident impedit!
           </p>
           <SearchBar />
-          <div className="boxes">
-            <div className="box">
-              <h1>16+</h1>
-              <h2>Years of Experience</h2>
-            </div>
-            <div className="box">
-              <h1>200</h1>
-              <h2>Award Gained</h2>
-            </div>
-            <div className="box">
-              <h1>2000+</h1>
-              <h2>Property Ready</h2>
-            </div>
-          </div>
         </div>
       </div>
       <div className="imgContainer">
